152bbe2945fe0ef29fc55dd29e75fd772d0f7c23
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-01-30 03:34:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,8 @@ var chocolateBars = ["snickers","hundred grand","kitcat","skittles"]
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
